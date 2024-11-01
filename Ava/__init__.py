b21c6b47385d967773bd7d395192c5404f54e6f5
DeadliestOne <uncountableaura@gmail.com>
2024-11-01 23:12:09 +0530

    Update __init__.py
    Jay Shree Krishna

--- Ava/__init__.py
+++ Ava/__init__.py
@@ -129,6 +129,8 @@ from Ava.modules.sql import SESSION
 telegraph = Telegraph()
 telegraph.create_account(short_name="Ava")
 
+defaults = tg.Defaults(run_async=True)
+
 updater = tg.Updater(
     token=TOKEN,
     base_url=BOT_API_URL,
